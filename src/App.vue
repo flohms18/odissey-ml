03eb080f6d7c270d5c1af47578f3f81e9f668b99
flohms18 <trouilletflorent@outlook.com>
2026-06-06 18:43:48 +0200

    Add Odissey FONT in CSS

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,21 @@ import Odissey from './components/Odissey.vue';
 
 <template>
   <Odissey></Odissey>
+
+  <h1 class="Main_Title">THIS IS MY ODISSEY FONT</h1>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+@font-face {
+  font-family: OdisseyFont;
+  src: url('./static/fonts/HomepageBaukasten-Bold.ttf');
+}
+
+.Main_Title {
+  color: #2F5E8D;
+  font-family: OdisseyFont, sans-serif;
+}
+
+
+</style>
