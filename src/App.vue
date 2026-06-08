4f900c9cdee34990c9e5459cf1c4c4d23ce76f98
flohms18 <trouilletflorent@outlook.com>
2026-06-08 14:12:50 +0200

    Add global css

--- src/App.vue
+++ src/App.vue
@@ -4,32 +4,13 @@ import Odissey from './components/Odissey.vue';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/global.css'
 
 </script>
 
 <template>
   <Odissey></Odissey>
-
-  <section>
-    <div class="d-flex bg-danger flex-row">
-      <div class="p-2">Flex item 1</div>
-      <div class="p-2">Flex item 2</div>
-      <div class="p-2">Flex item 3</div>
-    </div>
-  </section>
 </template>
 
 <style scoped>
-
-@font-face {
-  font-family: OdisseyFont;
-  src: url('./static/fonts/HomepageBaukasten-Bold.ttf');
-}
-
-.Main_Title {
-  color: #2F5E8D;
-  font-family: OdisseyFont, sans-serif;
-}
-
-
 </style>
--- src/components/Odissey.vue
+++ src/components/Odissey.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Footer from './Footer.vue';
 import Header from './Header.vue';
 
 
@@ -6,4 +7,18 @@ import Header from './Header.vue';
 
 <template>
     <Header></Header>
+    
+    <Footer></Footer>
 </template>
+
+<style scoped>
+
+:root {
+  --dark-color : #0D0D0D; 
+  --blue-color: #2F5E8D;
+  --red-color : #ce2127;
+}
+
+
+
+</style>
